c9adbd5113e1aac80dd7ff06bb07e62e3bb4f58c
Jason Swanepoel <Jason.Swanepoel@derivco.co.za>
2018-05-24 07:59:16 +0200

    heyy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule
+    NgbModule.forRoot()
   ],
-  providers: [
-    // no need to place any providers due to the `providedIn` flag...
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
